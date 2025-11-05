c36ea70e740cd191d8cc837cf5f59ed4ebce30d9
JaasperK <jasper.l.koehn@gmail.com>
2025-11-05 01:00:55 +0100

    updated stuff

--- simplex.py
+++ simplex.py
@@ -1,11 +1,19 @@
 import numpy as np
 
+def find_basis(tab):
+    basis = []
+    for j in range(tab.shape[1]):
+        col = tab[:, j]
+        if np.linalg.norm(col) == 1.0:
+            basis.append(j)
+    return basis
+
 def initial_tableau(c, A, b):
     t = np.vstack([-c, A])
     b = np.hstack([0, b])
     return np.column_stack([t, b])
 
-def simplex(c, A, b):
+def simplex(c, A, b, optimize="max"):
     """
     Solves linear programming problems in standard form, meaning all inequalities have been converted to equalities:
     ```
@@ -36,29 +44,29 @@ def simplex(c, A, b):
     tab = initial_tableau(c, A, b)
     m = A.shape[0]
     n = A.shape[1]
-    basis = np.arange(n - m, n)
-    while np.any(tab[0] < 0):   # solution is not optimal
+    while np.any(tab[0] < 0.0):  # solution is not optimal
         pivot_col = np.argmin(tab[0, :-1])
         r1 = tab[1:, pivot_col]
         r1 = np.where(r1 <= 0, -1, r1)  # avoid division by 0
         r2 = tab[1:, -1]
         ratio = np.where(r1 > 0, r2 / r1, np.inf)
-        pivot_row = np.argmin(ratio) + 1        # +1 to account for the z-row that was not included in r1 and r2
+        pivot_row = np.argmin(ratio) + 1  # +1 to account for the z-row that was not included in r1 and r2
 
-        # update basis
-        basis[basis == pivot_row + 1] = pivot_col
-
-        # update tableau
+        # update tableau, probably not necessary since pivot element is always 1 in our special case
         tab[pivot_row] = tab[pivot_row] / tab[pivot_row, pivot_col]
         
         rows = np.arange(tab.shape[0])
         for i in rows[rows != pivot_row]:
             tab[i] = tab[i] - tab[i, pivot_col] * tab[pivot_row]
 
-        print(tab)
     # find x using basis
+    basis = find_basis(tab)
     x = np.zeros(A.shape[1])
     for i in basis:
         x[i] = tab[:, i] @ tab[:, -1]
     
-    return x, tab[0][-1]
+    # probably not necessary since we only work with absolute values
+    if optimize == "max":
+        return x, tab[0][-1]
+    elif optimize == "min":
+        return x, -tab[0][-1]
--- test_cases.py
+++ test_cases.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from claspy.data_loader import load_tssb_dataset
 from simplex import simplex
 
 def case1():
@@ -17,7 +19,7 @@ def case1():
         [ 2, 1, 0, 0, 0, 1]
     ]).astype(np.float64)
     b = np.array([6, 3, 5, 4]).astype(np.float64)    
-    return c, A, b
+    return c, A, b, "max", 9.0
 
 def case2():
     c = np.array([7, 6, 0, 0]).astype(np.float64)
@@ -26,7 +28,7 @@ def case2():
         [3, 2, 0, 1]
     ]).astype(np.float64)
     b = np.array([16, 12])
-    return c, A, b
+    return c, A, b, "max", 32.0
 
 def case3():
     c = np.array([40, 30, 0, 0]).astype(np.float64)
@@ -35,7 +37,7 @@ def case3():
         [2, 1, 0, 1]
     ]).astype(np.float64)
     b = np.array([12, 16])
-    return c, A, b
+    return c, A, b, "max", 400.0
 
 def case4():
     c = np.array([3, 2, 0, 0, 0, 0]).astype(np.float64)
@@ -46,7 +48,7 @@ def case4():
         [0, 1, 0, 0, 0, 1]
     ]).astype(np.float64)
     b = np.array([9, 18, 7, 6])
-    return c, A, b
+    return c, A, b, "max", 22.5
 
 def case5():
     c = np.array([6, 9, 0, 0,]).astype(np.float64)
@@ -55,33 +57,74 @@ def case5():
         [1, 1, 0, 1]
     ]).astype(np.float64)
     b = np.array([12, 5])
-    return c, A, b
+    return c, A, b, "max", 36.0
 
 def case6():
-    c = np.array([-6, -9, 0, 0,]).astype(np.float64)
+    c = np.array([6, 9, 0, 0,]).astype(np.float64)
     A = np.array([
         [2, 3, 1, 0],
         [1, 1, 0, 1]
     ]).astype(np.float64)
     b = np.array([12, 5])
-    return c, A, b
+    return c, A, b, "min", 0.0
+
 
 def case7():
-    c = np.array([3, 4, 0, 0,]).astype(np.float64)
+    c = np.array([7, 3, 0]).astype(np.float64)
     A = np.array([
-        [1, 1, -1, 0],
-        [2, 1, 0, -1]
+        [2, -1, 0],
+        [1, 1, 1]
     ]).astype(np.float64)
-    b = np.array([450, 600])
-    return c, A, b
+    b = np.array([5, 9])
+    return c, A, b, "max", 45.669
+
+def load_dataset(dataset):
+    tssb = load_tssb_dataset([dataset])
+    idx, (dataset, window_size, cps, ts) = list(tssb.iterrows())[0]
+
+    window_size = 3
+    offset = 1
+
+    window1 = np.abs(ts[:window_size])               # is supplier, sum is ~4.1
+    window2 = np.abs(ts[offset:window_size+offset])  # is consumer, sum is ~3.1
+
+    print(window1)
+    print(f"\t {window2}")
+
+    c = np.zeros((window_size * window_size) + window_size, dtype=np.float64)
+    for i in range(window_size):
+        for j in range(window_size):
+            c[i * window_size + j] = np.abs(i - (offset + j))
+    print(c)
+
+    b_eq = window2
+    A_eq = np.zeros((window_size, window_size * window_size + window_size), dtype=np.float64)
+    for j in range(window_size):
+        for i in range(window_size):
+            A_eq[i, j * window_size + i] = 1.0
+
+    b_ub = window1
+    A_ub = np.zeros((window_size, window_size * window_size + window_size), dtype=np.float64)
+    for i in range(window_size):
+        for j in range(window_size):
+            A_ub[i, i * window_size + j] = 1.0
+        A_ub[i, window_size * window_size + i] = 1.0
+
+    b = np.hstack([b_eq, b_ub])
+    A = np.vstack([A_eq, A_ub])
+
+    return c, A, b, "min", -5.116696
 
 
 def main():
-    cases = [case1, case2, case3, case4, case5, case6, case7]
-    for case in cases:
-        c, A, b = case()
-        x, z = simplex(c, A, b)
-        print(f"Solution x: {x}\nCosts: {c}\nObjective value z = {z}.\n")
+    c, A, b, optimize, z_true = load_dataset("Adiac")
+    x, z_pred = simplex(c, A, b, optimize)
+    z_pred = np.round(z_pred, 6)
+    print(f"Solution x: {x}\nCosts: {c}\nObjective value z: {z_pred} \nDiff of pred and true z = {z_pred - z_true}.\n")
     
 if __name__ == "__main__":
+    import shutil
+    width = shutil.get_terminal_size().columns
+    np.set_printoptions(linewidth=width)
+
     main()
